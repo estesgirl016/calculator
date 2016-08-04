4aac80c9aec53c54c10abcabb3ec12b5833d2b8b
Carrie Estes <my4countrygirl@gmail.com>
2016-08-04 09:25:11 -0600

    basic calculator

--- calculator.rb
+++ calculator.rb
@@ -1,35 +1,58 @@
 def first_number
-  puts "What is the first number?"
-  first_number = gets.to_i
+  print "What is the first number? "
+  @first_number = gets.to_i
 end
 
 def next_number
-  puts "What is the next number?"
-  next_number = gets.to_i
+  print "What is the next number? "
+  @next_number = gets.to_i
 end
 
 
-# op = ['+','-','*','%','/']
 
 def operator
-  puts "Operator you would like to use? (+,-,*,%,/)"
-  operator_value = gets.strip
-  # need case statement
-  # need error message
-  # need to add array
-  # need to make % 0 impossible
-  #case
-  #else
+  print "Operator you would like to use? (+,-,*,/) "
+  @operator_value = gets.strip
+  # @operator_value
+end
+
+def calculate
+  @result = case @operator_value
+  when '+'
+    @first_number + @next_number
+
+  when '-'
+    @first_number - @next_number
+
+  when '*'
+    @first_number * @next_number
+
+  when '/'
+    if @next_number == 0
+      puts "Can't divide by 0"
+      calculator
+    else
+      @first_number / @next_number
+    end
+
+  else
+    put "Invalid operation. Please try again."
+    calculator
   end
+end
+
 #
 def calculator
     puts "Welcome to the Ruby Calculator."
     first_number
     operator
     next_number
+    calculate
+    puts "#{@first_number} #{@operator_value} #{@next_number} is #{@result}"
+
+    # if we can't show error message and ask for new equation
 end
 
-  #
 
 
  calculator
